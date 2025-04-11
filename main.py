4d324a8efdc372a0bdda0ec697500aa13b07c4fa
Prashant Ronad <prashantronad870@gmail.com>
2025-04-11 10:10:40 +0530

    add

---
+++ main.py
@@ -0,0 +1,77 @@
+from fastapi import FastAPI, HTTPException, Depends
+import uvicorn
+from typing import Annotated, List
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from database import SessionLocal, engine
+import models
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+origins = [
+    'http://localhost:5173',
+    
+
+]
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   expose_headers=["*"]
+                   )
+
+class TransactionBase(BaseModel):
+    amount: float
+    category: str
+    description: str
+    is_income: bool
+    date: str
+
+class TransactionModel(TransactionBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+models.Base.metadata.create_all(bind=engine)
+
+@app.post("/transactions/", response_model=TransactionModel)
+async def create_transaction(transaction: TransactionBase, db: db_dependency):
+    db_transaction = models.Transaction(**transaction.dict())
+    db.add(db_transaction)
+    db.commit()
+    db.refresh(db_transaction)
+    return db_transaction
+
+
+@app.get("/transactions/",response_model= List[TransactionModel])
+async def get_transactions(db: db_dependency):
+    transactions = db.query(models.Transaction).all()
+    return transactions
+
+
+@app.delete("/transactions/{transaction_id}")
+async def delete_transaction(transaction_id: int, db: db_dependency):
+    db_transaction = db.query(models.Transaction).filter(models.Transaction.id == transaction_id).first()
+    if not db_transaction:
+        raise HTTPException(status_code=404, detail="Transaction not found")
+    db.delete(db_transaction)
+    db.commit()
+    return {"message": "Transaction deleted successfully"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port = 8000)
